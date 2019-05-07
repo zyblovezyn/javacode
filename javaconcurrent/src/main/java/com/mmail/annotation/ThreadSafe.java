c685df49b856f21d4ae0673ce074827fbb222251
zyb@home <love.zyb@outlook.com>
2019-05-07 21:34:22 +0800

    'annotation'

---
+++ javaconcurrent/src/main/java/com/mmail/annotation/ThreadSafe.java
@@ -0,0 +1,15 @@
+package com.mmail.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程安全的类或写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE) //如果在项目中使用，使用RUNTIME
+public @interface ThreadSafe {
+    String value() default "";
+}
