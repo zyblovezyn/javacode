37226335f64222616126643a6381211a45a50d11
zhangyuanbo@office <luoyiguren@outlook.com>
2019-05-24 16:56:52 +0800

    add shellsort

--- algorithm/排序算法.java
+++ algorithm/排序算法.java
@@ -1,16 +1,36 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 // 常用排序
 class Sort {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		int[] arr = new int[] { 5, 4, 3, 6, 7, 8, 1, 9 };
 		// bubbleSort(arr);
-		//quickSort(arr, 0, arr.length - 1);
-		
-		//insertionSort(arr);
-		//for (int var : arr) {
-			//System.out.println(var);
-		//}
-		System.out.println(Integer.toBinaryString(15));
+		// quickSort(arr, 0, arr.length - 1);
+
+		// insertionSort(arr);
+		shellSort(arr);
+		for (int var : arr) {
+		System.out.println(var);
+		}
+		//System.out.println(Integer.toBinaryString(15));
+		Set<String> set=new HashSet<Strict>();
+		for(String s:set.toArray()){
+
+		}
+
+		Iterator<String> it=set.iterator();
+		while(it.hasNext()){
+			Strict s=it.next();
+		}
+
+		// 增强for
+		for(String s:set){
+
+		}
 	}
 
 	// 冒泡排序O(N^2)
@@ -24,48 +44,6 @@ class Sort {
 					A[j] = temp;
 				}
 			}
-		}
-	}
-
-	// 快速排序
-	public static void quickSort(int[] arr, int start, int end) {
-		/*
-		if (start < end) {
-			int baseValue = arr[start]; // 基准数
-			int low = start;
-			int high = end;
-			while (low < high) {
-				while (low < high && arr[high] >= baseValue) {
-					high--;
-				}
-				arr[low] = arr[high];
-				while (low < high && arr[low] <= baseValue) {
-					low++;
-				}
-				arr[high] = arr[low];
-			}
-			arr[low] = baseValue;
-			quickSort(arr, start, low);
-			quickSort(arr, low + 1, end);
-		}
-		*/
-		if (start < end) {
-			int baseValue = arr[start]; // 基准数
-			int low = start;
-			int high = end;
-			while (low < high) {
-				while (low < high && arr[high] >= baseValue) {
-					high--;
-				}
-				arr[low] = arr[high];
-				while (low < high && arr[low] <= baseValue) {
-					low++;
-				}
-				arr[high] = arr[low];
-			}
-			arr[low] = baseValue;
-			quickSort(arr, start, low);
-			quickSort(arr, low + 1, end);
 		}
 	}
 
@@ -102,20 +80,52 @@ class Sort {
 		}
 	}
 
+	// 快速排序 time O(N*logN)
+	public static void quickSort(int[] arr, int start, int end) {
+		/*
+		 * if (start < end) { int baseValue = arr[start]; // 基准数 int low = start; int
+		 * high = end; while (low < high) { while (low < high && arr[high] >= baseValue)
+		 * { high--; } arr[low] = arr[high]; while (low < high && arr[low] <= baseValue)
+		 * { low++; } arr[high] = arr[low]; } arr[low] = baseValue; quickSort(arr,
+		 * start, low); quickSort(arr, low + 1, end); }
+		 */
+		if (start < end) {
+			int baseValue = arr[start]; // 基准数
+			int low = start;
+			int high = end;
+			while (low < high) {
+				while (low < high && arr[high] >= baseValue) {
+					high--;
+				}
+				arr[low] = arr[high];
+				while (low < high && arr[low] <= baseValue) {
+					low++;
+				}
+				arr[high] = arr[low];
+			}
+			arr[low] = baseValue;
+			quickSort(arr, start, low);
+			quickSort(arr, low + 1, end);
+		}
+	}
+
+	// 希尔排序
 	public static void shellSort(int[] arr) {
+		// 定义步长
 		int len = arr.length;
-		for (int gap = (int) Math.floor(len / 2); gap > 0; gap = (int) Math.floor(gap / 2)) {
+		for (int gap = len / 2; gap > 0; gap=gap/2) {
 			// 注意：这里和动图演示的不一样，动图是分组执行，实际操作是多个分组交替执行
 			for (int i = gap; i < len; i++) {
-				int j = i;
-				int current = arr[i];
-				while (j - gap >= 0 && current < arr[j - gap]) {
-					arr[j] = arr[j - gap];
-					j = j - gap;
-				}
-				arr[j] = current;
+				// 遍历本组所有元素
+				  for(int j=i-gap;j>=0;j-=gap){
+					  if(arr[j]>arr[j+gap]){
+						  int temp=arr[j];
+						  arr[j]=arr[j+gap];
+						  arr[j+gap]=temp;
+					  }
+				  }
 			}
+			//System.out.println(Arrays.toString(arr));
 		}
-		return;
 	}
 }
